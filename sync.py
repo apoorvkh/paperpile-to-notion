ee68b5fa7afdb9eb6d891908c1cd130c09a9c370
Apoorv Khandelwal <mail@apoorvkh.com>
2023-02-19 23:52:45 +0000

    Working

--- sync.py
+++ sync.py
@@ -1,6 +1,4 @@
 import os
-import requests
-
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 
@@ -14,11 +12,13 @@ DATABASE_IDENTIFIER = os.environ["DATABASE_IDENTIFIER"]
 ## User customizations
 
 BIB_PATH = "references.bib"
+
+TITLE_KEY = "Title"
 PRIMARY_KEY = "Reference ID"
 
 # Notion column names : Notion property types
 notion_property_types = {
-    # "Title": "title",
+    "Title": "title",
     "Authors": "rich_text",
     "Year": "rich_text",
     "Reference ID": "rich_text",
@@ -27,7 +27,7 @@ notion_property_types = {
 
 # Notion column names : Bibtex property keys
 notion_property_keys = {
-    # "Title": "title",
+    "Title": "title",
     "Authors": "author",
     "Year": "year",
     "Reference ID": "ID",
@@ -38,35 +38,34 @@ notion_property_keys = {
 
 if __name__ == "__main__":
 
-    # Parse Bibtex
+    print(f"Parsing {BIB_PATH} ...")
 
     parser = BibTexParser(
-        customization = bibtex_parsing.parser_customizations,
-        ignore_nonstandard_types = False
+        customization=bibtex_parsing.parser_customizations,
+        ignore_nonstandard_types=False,
     )
 
-    with open(BIB_PATH) as bibtex_file:
-        bib_database = bibtexparser.load(bibtex_file, parser=parser)
-
-    # Format: "content_dict"
+    bib_database = bibtexparser.load(open(BIB_PATH), parser=parser)
     bib_entries = [
         bibtex_parsing.bibtex_to_content_dict(e, notion_property_keys)
         for e in bib_database.entries
     ]
 
-    # Query Notion DB
+    print("Querying Notion DB ...")
 
     notion_headers = notion.get_headers(NOTION_TOKEN)
 
     notion_pages = notion.query_db(db_id=DATABASE_IDENTIFIER, headers=notion_headers)
-    notion_page_ids = [page['id'] for page in notion_pages]
+
+    notion_page_ids = [page["id"] for page in notion_pages]
     notion_entries = [
         notion.page_to_entry(page, property_types=notion_property_types)
         for page in notion_pages
     ]
-
-    notion_entries_by_pkey = { e[PRIMARY_KEY] : e for e in notion_entries }
-    notion_page_ids_by_pkey = { e[PRIMARY_KEY] : page_id for e, page_id in zip(notion_entries, notion_page_ids)}
+    notion_entries_by_pkey = {e[PRIMARY_KEY]: e for e in notion_entries}
+    notion_page_ids_by_pkey = {
+        e[PRIMARY_KEY]: page_id for e, page_id in zip(notion_entries, notion_page_ids)
+    }
 
     # Insert or update missing pages
 
@@ -75,10 +74,22 @@ if __name__ == "__main__":
 
         if p_key not in notion_entries_by_pkey:
             notion_page_id = None
-        elif bib_e != notion_entries_by_pkey[PRIMARY_KEY]:
-            notion_page_id = notion_page_ids_by_pkey[PRIMARY_KEY]
+        elif bib_e != notion_entries_by_pkey[p_key]:
+            notion_page_id = notion_page_ids_by_pkey[p_key]
         else:
             continue
 
+        print("Updating" if notion_page_id else "Adding", end="")
+        print(f' page for paper "{bib_e[TITLE_KEY]}"')
+
         properties = notion.entry_to_properties(bib_e, notion_property_types)
-        notion.update_page(db_id=DATABASE_IDENTIFIER, properties=properties, update_page_id=notion_page_id, headers=notion_headers)
+
+        response_ok, response_text = notion.update_page(
+            db_id=DATABASE_IDENTIFIER,
+            properties=properties,
+            update_page_id=notion_page_id,
+            headers=notion_headers,
+        )
+
+        if response_ok is False:
+            print(response_text)
--- utils/notion.py
+++ utils/notion.py
@@ -21,7 +21,7 @@ def query_db(db_id, headers=None):
     pages = []
 
     while response["has_more"]:
-        response = requests.post(url, json=payload, headers=headers, timeout=10).json()
+        response = requests.post(url, json=payload, headers=headers, timeout=60).json()
         pages += response["results"]
         payload["start_cursor"] = response["next_cursor"]
 
@@ -38,14 +38,12 @@ def update_page(db_id, properties, update_page_id=None, headers=None):
 
     if update_page_id is not None:
         url = f"{NOTION_API_BASE_URL}/pages/{update_page_id}"
-        response = requests.patch(url, json=payload, headers=headers, timeout=10)
+        response = requests.patch(url, json=payload, headers=headers, timeout=60)
     else:
         url = f"{NOTION_API_BASE_URL}/pages"
-        response = requests.post(url, json=payload, headers=headers, timeout=10)
+        response = requests.post(url, json=payload, headers=headers, timeout=60)
 
-    print(response.json())
-
-    return response.ok
+    return response.ok, response.text
 
 
 def page_to_entry(page, property_types):
@@ -68,35 +66,29 @@ def page_to_entry(page, property_types):
     return content_dict
 
 
-def content_to_property_value(property_type, content):
+def string_to_property_value(property_type, content):
     if property_type == "title":
         return {
-            "id": "title",
-            "title": [
+            "title": [{
+                "text": { "content": content }
+            }]
+        }
+    elif property_type == "rich_text":
+        return {
+            "rich_text": [
                 {
+                    "text": {"content": content[:2000]},
                     "type": "text",
-                    "text": {
-                        "content": content,
-                    }
                 }
             ]
         }
-    elif property_type == "rich_text":
-        return [
-            {
-                "type": "text",
-                "text": {
-                    "content": content,
-                },
-            }
-        ]
-    elif property_type in ["number", "url"]:
+    elif property_type == "url":
         return {property_type: content}
     return {property_type: content}
 
 
 def entry_to_properties(entry, property_types):
     return {
-        name: content_to_property_value(property_types[name], content)
+        name: string_to_property_value(property_types[name], content)
         for name, content in entry.items()
     }
